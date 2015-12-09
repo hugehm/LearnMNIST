c6d9c2a0a525d2b4efc971fa2570bef02d3f16e2
hugehm <gennad92@mail.ru>
2015-12-09 17:34:13 +0300

    Add metrics function

--- learnModel.R
+++ learnModel.R
@@ -18,7 +18,8 @@ learnModel <- function(data, labels){
   for (i in 1:num_labels){
     temp <- i-1
     print(i)
-    buff <- optim(initial_theta, lrCostFunction, gr = grlrCostFunction, X=X, y=(y==temp), lambda=lambda, method = "CG")
+    buff <- optim(initial_theta, lrCostFunction, gr = grlrCostFunction, X=X, y=(y==temp), 
+                  lambda=lambda, method = "CG")
     all_theta[i,] <- buff$par
   }
   
